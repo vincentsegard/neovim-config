77e1bfac0ecf1599dd3a90b16873dd17e7b0dbf3
Vincent SEGARD <segard.vincent@gmail.com>
2023-12-22 00:52:38 +0100

    theme darker

--- init.lua
+++ init.lua
@@ -190,10 +190,12 @@ require('lazy').setup({
   },
 
   {
-    -- Theme inspired by Atom
     'navarasu/onedark.nvim',
     priority = 1000,
     config = function()
+      require('onedark').setup {
+        style = 'darker'
+      }
       vim.cmd.colorscheme 'onedark'
     end,
   },
