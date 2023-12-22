50bb3d42af39d6c95e56e8996a744f3b08b4bc6d
Vincent SEGARD <segard.vincent@gmail.com>
2023-12-22 11:36:52 +0100

    clean plugins && adding neo-tree

--- init.lua
+++ init.lua
@@ -261,14 +261,13 @@ require('lazy').setup({
   --       Uncomment any of the lines below to enable them.
   -- require 'kickstart.plugins.autoformat',
   -- require 'kickstart.plugins.debug',
-
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    You can use this folder to prevent any conflicts with this init.lua if you're interested in keeping
   --    up-to-date with whatever is in the kickstart repo.
   --    Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
   --
   --    For additional information see: https://github.com/folke/lazy.nvim#-structuring-your-plugins
-  { import = 'custom.plugins' },
+  { import = 'ninange.plugins' },
 }, {})
 
 -- [[ Setting options ]]
@@ -661,3 +660,11 @@ cmp.setup {
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
+vim.api.nvim_create_autocmd("BufReadPost", {
+  pattern = "*",
+  callback = function()
+    vim.cmd("Neotree filesystem reveal left")
+  end,
+  group = vim.api.nvim_create_augroup("auto_open_neotree", { clear = true }),
+  desc = "Auto open Neo-tree when entering buffer",
+})
---
+++ lua/ninange/plugins/mykeymaps/init.lua
@@ -0,0 +1,18 @@
+function Map(mode, lhs, rhs, opts)
+    local options = { noremap = true, silent = true }
+    if opts then
+        options = vim.tbl_extend('force', options, opts)
+    end
+    vim.keymap.set(mode, lhs, rhs, options)
+end
+
+return {
+    dir = "/home/ninange/.config/nvim/lua/ninange/plugins/mykeymaps",
+    lazy = false,
+    config = function()
+        vim.g.mapleader = ' '
+        Map('n', '<C-down>', '1<C-w>+')
+        Map('n', '<C-up>', '1<C-w>-')
+        Map('n', '<A-w>', '<C-w>')
+    end
+}
