2b3606c1501ea16e8f3e3a783fd1754cbe4b6f9c
Vincent SEGARD <segard.vincent@gmail.com>
2023-12-23 00:00:00 +0100

    adding buffer in bottom & icon (like a point when file edited and not saved

--- init.lua
+++ init.lua
@@ -206,10 +206,15 @@ require('lazy').setup({
     -- See `:help lualine.txt`
     opts = {
       options = {
-        icons_enabled = false,
+        icons_enabled = true,
         theme = 'onedark',
         component_separators = '|',
         section_separators = '',
+      },
+      sections = {
+        lualine_a = {
+          'buffers',
+        },
       },
     },
   },
