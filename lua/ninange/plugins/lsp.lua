5aee77b8a95ef07433cc7b2eee85b7600b5c838a
Vincent SEGARD <segard.vincent@gmail.com>
2023-12-30 12:56:39 +0100

    constitent: alt to switch, ctrl to create

--- lua/ninange/plugins/lsp.lua
+++ lua/ninange/plugins/lsp.lua
@@ -62,7 +62,7 @@ return {
 
     -- document existing key chains
     require('which-key').register {
-      ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
+      ['<leader>c'] = { name = '[C]ode'},
       ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
       ['<leader>g'] = { name = '[G]it', _ = 'which_key_ignore' },
       ['<leader>h'] = { name = 'Git [H]unk', _ = 'which_key_ignore' },
