aa4bebc747a673ba9f2b91101df9ad3adb3351ce
Vincent SEGARD <segard.vincent@gmail.com>
2024-01-29 18:38:40 +0100

    removing ruby_ls

--- lua/ninange/plugins/lsp.lua
+++ lua/ninange/plugins/lsp.lua
@@ -127,7 +127,7 @@ return {
 					},
 				},
 			},
-			setup = {},
+		setup = {},
 		},
 		init = function()
 			-- [[ Configure LSP ]]
@@ -167,22 +167,21 @@ return {
 				nmap('<leader>wr', vim.lsp.buf.remove_workspace_folder, '[W]orkspace [R]emove Folder')
 				nmap('<leader>wl', function()
 					print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-				end, '[W]orkspace [L]ist Folders')
+					end, '[W]orkspace [L]ist Folders')
 
 				-- Create a command `:Format` local to the LSP buffer
 				vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
 					vim.lsp.buf.format()
-				end, { desc = 'Format current buffer with LSP' })
+					end, { desc = 'Format current buffer with LSP' })
 			end
 
-					local servers = {
+			local servers = {
 				-- clangd = {},
 				-- gopls = {},
 				-- pyright = {},
 				-- rust_analyzer = {},
 				-- tsserver = {},
 				-- html = { filetypes = { 'html', 'twig', 'hbs'} },
-        ruby_ls = {},
 
 				lua_ls = {
 					Lua = {
