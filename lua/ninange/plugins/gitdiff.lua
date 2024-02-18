98b0099b190f931751b8eff628f45d2ad05e82b4
Vincent SEGARD <segard.vincent@gmail.com>
2024-02-18 07:10:19 +0100

    adding todo-comments & codium -commented need to read cgu-

--- lua/ninange/plugins/gitdiff.lua
+++ lua/ninange/plugins/gitdiff.lua
@@ -12,6 +12,6 @@ return {
     })
     vim.keymap.set('n', '<leader>f', '<CMD>DiffviewFileHistory %<CR>', { desc = 'Git history [F]ile' })
     vim.keymap.set('n', '<leader>g', '<CMD>DiffviewFileHistory<CR>', { desc = '[G]it history all' })
-    vim.keymap.set('n', '<leader>o', '<CMD>DiffviewOpen<CR>', { desc = '[Open] git diff' })
+    vim.keymap.set('n', '<leader>o', '<CMD>DiffviewOpen<CR>', { desc = '[O]pen git diff' })
   end
 }
